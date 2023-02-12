31386ad34451b59b1b760fffdbe95e68104336b6
Deon Achuo <sird655@gmail.com>
2023-02-12 02:50:03 +0100

    refactored login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,124 +1,111 @@
 import User from "../models/user";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import _ from "lodash"
-import bcrypt from "bcryptjs"
+import _ from "lodash";
+import bcrypt from "bcryptjs";
 
-dotenv.config()
+dotenv.config();
 
 class UserController {
-    async signUp(args: any) {
-      // Check if the user exists
-      const user = await User.find({ phone: args.phone })
-      if (user.length >= 1) {
-        return {response: {
+  async signUp(args: any) {
+    // Check if the user exists
+    const user = await User.find({ phone: args.phone });
+    if (user.length >= 1) {
+      return {
+        response: {
           message: "User already exists",
-          status: "failed"
-        }}
-      } 
+          status: "failed",
+        },
+      };
+    }
 
-      // Create a new user
-     try {
-      const hash = await bcrypt.hash(args.password, 10)
+    // Create a new user
+    try {
+      const hash = await bcrypt.hash(args.password, 10);
       const newUser = new User({
         fname: args.fname,
         lname: args.lname,
         email: args.email,
         phone: args.phone,
-        password: hash
+        password: hash,
       });
-      const result = await  newUser.save()
+      const result = await newUser.save();
       const token: string = jwt.sign(
-            {
-              userId: result._id,
-              email: result.email,
-              phone: result.phone,
-            },
-            process.env.JWT_SECRET as string,
-            {
-              expiresIn: "1h",
-            }
-          );
+        {
+          userId: result._id,
+          email: result.email,
+          phone: result.phone,
+        },
+        process.env.JWT_SECRET as string
+      );
       return {
-            response:{   
-            message: "Account created",
-            token: token,
-            status: "success"
-          }
-          };
-      
-     } catch (error:any) {
-        return {
-          response:{
-            message: error.message,
-          }
-        };
-     }
+        response: {
+          message: "Account created",
+          token: token,
+          status: "success",
+        },
+      };
+    } catch (error: any) {
+      return {
+        response: {
+          message: error.message,
+        },
+      };
     }
+  }
 
+  async login(args: any) {
+    try {
+      const user = await User.find({ phone: args.phone });
 
-  async login(args:any){
-   return User.find({ phone: args.phone, })
-    .exec()
-    .then((user) => {
       if (user.length < 1) {
         return {
-          response:{
-            message: "Authentication Failed",
-          }         
-         }
+          response: {
+            message: "User not found !!",
+            status: "failed",
+          },
+        };
       }
 
-      bcrypt.compare(
-        args.password,
-        user[0].password,
-        (err: any, result: any) => {
-          if (err) {
-            return {
-              response:{
-                message: "Authentication Failed",
-              }
-            }
-          }
-          if (result) {
-            const token: string = jwt.sign(
-              {
-                userId: result[0]._id,
-                email: result[0].email,
-                phone: result[0].phone,
-              },
-              process.env.JWT_SECRET as string,
-              {
-                expiresIn: "1h",
-              }
-            );
-            return {
-              response:{
-                message: "Login Successful",
-                token: token,
-              }
-            }
-          }
-         return {
-          response:{message: "Authentication Failed",}
-         }
-        }
-      );
-    })
-    .catch((err) => {
+      // Compare password
+      const result = await bcrypt.compare(args.password, user[0].password);
+      if (result) {
+        const token: string = jwt.sign(
+          {
+            userId: user[0]._id,
+            email: user[0].email,
+            phone: user[0].phone,
+          },
+          process.env.JWT_SECRET as string
+        );
+        return {
+          response: {
+            message: "Login Successful",
+            token: token,
+            status: "success",
+          },
+        };
+      } else {
+        return {
+          response: {
+            message: "Authentication failed !!",
+            status: "failed",
+          },
+        };
+      }
+    } catch (error) {
       return {
-        response:{
-          message: err.message,
-        }
+        response: {
+          message: "Authentication failed !!",
+          status: "failed",
+        },
       };
-    });
+    }
   }
-  async update(args:any){}
-  async  forgotPassword(args:any){}
-  async resetPassword(args:any){}
-  async  updatePassword(args:any){}
-
+  async update(args: any) {}
+  async forgotPassword(args: any) {}
+  async resetPassword(args: any) {}
+  async updatePassword(args: any) {}
 }
-
 
 export default UserController;

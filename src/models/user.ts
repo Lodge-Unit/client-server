f9b9cf13d5c8d767aeea36a6eb5d13e0479eb19c
Enow Divine <sirdivine16@gmail.com>
2023-03-19 17:21:23 +0100

    update user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    status: {
+      type: String,
+      default: "approved",
+    },
     password: {
       type: String,
       required: true,
